432b2134a2e316e0ed3b00432b955595123a1304
Almaz Esengulov <almaz.esengulov.pi9.03.20@gmail.com>
2025-06-03 19:57:39 +0600

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,34 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript", "plugin:import/recommended"),
+
+  {
+    rules: {
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "eqeqeq": ["error", "always"],
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+      "no-var": "error",
+      "prefer-const": "error",
+      "arrow-body-style": ["error", "as-needed"],
+      "react/jsx-uses-react": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
+      "import/order": [
+        "error",
+        {
+          "groups": ["builtin", "external", "internal", "parent", "sibling", "index"],
+          "alphabetize": { "order": "asc", "caseInsensitive": true },
+          "newlines-between": "always"
+        }
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
